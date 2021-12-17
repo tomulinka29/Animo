c8608a6991efa89eb56a7b10fc475cb47b206222
Tomáš Brůna <bruntoma@outlook.cz>
2021-12-17 19:46:26 +0100

    new branch, added custom list

---
+++ List/List/List.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    class List
+    {
+        int[] items;
+        int count = 0;
+
+        public List()
+        {
+            items = new int[0];
+        }
+
+        public void Reinit(int size)
+        {
+            //realloc
+            Array.Resize(ref items, size);
+        }
+
+        public void Add(int newItem)
+        {
+            if (count >= items.Length)
+            {
+                Reinit(count + 5);
+            }
+
+            items[count] = newItem; 
+            count++;
+        }
+
+        public void RemoveLast()
+        {
+            Reinit(count - 1);
+            count--;         
+        }
+
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < count; i++)
+                if (items[i] == item)
+                    return true;
+            return false;
+        }
+
+        public void Reverse()
+        {
+            int a = 0;       // first element to swap
+            int b = count-1; // with this element
+            int temp = 0;
+            int i = 0; // we only want to go to the half of the array
+
+            while(i < count)
+            {
+                temp = items[a];
+                items[a] = items[b];
+                items[b] = temp;
+                b--;
+                a++;
+                i += 2;
+            }
+        }
+
+   
+
+        public int this[int index] => items[index];
+
+        public override string ToString()
+        {
+            string text = "";
+            for (int i = 0; i < count; i++)
+                text += items[i].ToString();
+            return text;
+        }
+    }
+}
---
+++ List/List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(6);
+            list.Add(6);
+            list.Add(6);
+            list.Add(5);
+            list.Add(4);
+            list.Add(3);
+
+            Console.WriteLine(list.ToString());
+            list.Reverse();
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
